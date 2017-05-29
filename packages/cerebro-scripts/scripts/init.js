2e5cfa80c88f828a2d55c0be87c0ca2d75e21d9b
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-30 00:03:38 +0200

    remove gitignore renaming from template step

--- packages/cerebro-scripts/scripts/init.js
+++ packages/cerebro-scripts/scripts/init.js
@@ -50,26 +50,6 @@ module.exports = function(appPath, appName, originalDirectory) {
     return;
   }
 
-  // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
-  // See: https://github.com/npm/npm/issues/1862
-  fs.move(
-    path.join(appPath, 'gitignore'),
-    path.join(appPath, '.gitignore'),
-    [],
-    err => {
-      if (err) {
-        // Append if there's already a `.gitignore` file there
-        if (err.code === 'EEXIST') {
-          const data = fs.readFileSync(path.join(appPath, 'gitignore'));
-          fs.appendFileSync(path.join(appPath, '.gitignore'), data);
-          fs.unlinkSync(path.join(appPath, 'gitignore'));
-        } else {
-          throw err;
-        }
-      }
-    }
-  );
-
   // Display the most elegant way to cd.
   // This needs to handle an undefined originalDirectory for
   // backward compatibility with old global-cli's.
