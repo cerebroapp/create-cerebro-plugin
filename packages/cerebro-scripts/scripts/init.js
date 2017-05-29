72122bcd3b02ae34fb564ae79ffa674b08d20d34
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-29 22:44:16 +0200

    Update cerebro-scripts/init.js

--- packages/cerebro-scripts/scripts/init.js
+++ packages/cerebro-scripts/scripts/init.js
@@ -1,3 +1,95 @@
-module.exports = function() {
-  console.log('Start initialization of cerebro-scripts')
+'use strict';
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+const fs = require('fs-extra');
+const path = require('path');
+const chalk = require('chalk');
+const spawn = require('react-dev-utils/crossSpawn');
+
+module.exports = function(appPath, appName, originalDirectory) {
+  const ownPackageName = require(path.join(
+    __dirname,
+    '..',
+    'package.json'
+  )).name;
+  const ownPath = path.join(appPath, 'node_modules', ownPackageName);
+  const appPackage = require(path.join(appPath, 'package.json'));
+
+  // Copy over some of the devDependencies
+  appPackage.dependencies = appPackage.dependencies || {};
+  appPackage.devDependencies = appPackage.devDependencies || {};
+
+  appPackage.main = 'dist/index.js';
+
+  appPackage.keywords = ["cerebro", "cerebro-plugin"]
+
+  // Setup the script rules
+  appPackage.scripts = {
+    start: 'cerebro-scripts start',
+    build: 'cerebro-scripts build',
+    test: 'cerebro-scripts test'
+  };
+
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2)
+  );
+
+  // Copy the files for the user
+  const templatePath = path.join(ownPath, 'template');
+  if (fs.existsSync(templatePath)) {
+    fs.copySync(templatePath, appPath);
+  } else {
+    console.error(
+      `Could not locate supplied template: ${chalk.green(templatePath)}`
+    );
+    return;
+  }
+
+  // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
+  // See: https://github.com/npm/npm/issues/1862
+  fs.move(
+    path.join(appPath, 'gitignore'),
+    path.join(appPath, '.gitignore'),
+    [],
+    err => {
+      if (err) {
+        // Append if there's already a `.gitignore` file there
+        if (err.code === 'EEXIST') {
+          const data = fs.readFileSync(path.join(appPath, 'gitignore'));
+          fs.appendFileSync(path.join(appPath, '.gitignore'), data);
+          fs.unlinkSync(path.join(appPath, 'gitignore'));
+        } else {
+          throw err;
+        }
+      }
+    }
+  );
+
+  console.log();
+  console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log('Inside that directory, you can run several commands:');
+  console.log();
+  console.log(chalk.cyan(`  yarn start`));
+  console.log('    Starts the development process of plugin.');
+  console.log();
+  console.log(
+    chalk.cyan(`  yarn build`)
+  );
+  console.log('    Build your plugin');
+  console.log();
+  console.log(chalk.cyan(`  yarn test`));
+  console.log('    Starts the test runner.');
+  console.log();
+  console.log('We suggest that you begin by typing:');
+  console.log();
+  console.log(chalk.cyan('  cd'), cdpath);
+  console.log(`  ${chalk.cyan(`yarn start`)}`);
+  console.log();
+  console.log('Happy hacking!');
 }
