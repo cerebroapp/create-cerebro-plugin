f4370f8fc1869173167641cdc2b83bfca658b16b
dubisdev <dubisdev@gmail.com>
2022-06-30 17:24:08 +0200

    chore: spawn jest in subthread to support esm

--- packages/cerebro-scripts/scripts/runtest.js
+++ packages/cerebro-scripts/scripts/runtest.js
@@ -1,29 +1,19 @@
-'use strict';
-
-process.env.BABEL_ENV = 'test';
-process.env.NODE_ENV = 'test';
-process.env.PUBLIC_URL = '';
+import createJestConfig from './utils/createJestConfig.js';
+import path from 'path';
+import spawn from 'cross-spawn'
+import paths from '../config/paths.js';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-  throw err;
-});
+process.on('unhandledRejection', err => { throw err });
 
-import jest from 'jest'
 const argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
-
-// @remove-on-eject-begin
-// This is not necessary after eject because we embed config into package.json.
-import createJestConfig from './utils/createJestConfig.js';
-import path from 'path';
-import paths from '../config/paths.js';
 
 argv.push(
   '--config',
@@ -34,4 +24,9 @@ argv.push(
     )
   )
 );
-jest.run(argv);
+
+spawn.sync(
+  "node --experimental-vm-modules node_modules/jest/bin/jest.js", 
+  argv,
+  { stdio: 'inherit' }
+)
